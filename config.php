751f00bb36e91fac6d012d9934a47d2e3e639dcb
dark-pixels <gps.vr2.in@gmail.com>
2025-08-13 21:44:53 +0530

    Fix header output issue in config.php

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
+<?php
 define('DB_DRIVER', getenv('DB_DRIVER'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOSTNAME', getenv('DB_HOSTNAME'));
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_PORT', getenv('DB_PORT'));
+
+define('LDAP_AUTH', false);
+define('REMEMBER_ME_AUTH', true);
